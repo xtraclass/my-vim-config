b021d6d65b7b3a27d49c4cd834b268f2778940c4
Thomas Eichberger <thomas.eichberger@gmail.com>
2013-04-06 10:57:57 +0200

    install script should only create a link.

--- install.sh
+++ install.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
 cd
-cd .vim
-mkdir bundle &>/dev/null
-mkdir autoload &>/dev/null
-rm .vimrc &>/dev/null
-
-curl -Sso autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
-#cat > .vimrc <<END---
-#set nocompatible
-#
-#execute pathogen#infect()
-#
-#syntax on
-#
-#filetype plugin indent on
-#
-#END---
-
-cd
 rm .vimrc
 ln -s .vim/.vimrc .vimrc
 cd .vim
